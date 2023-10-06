cfde2a43a6890b8402ea4de41cc25d1b2b8b4dea
asrafulsow <aisowrav11@gmail.com>
2023-10-06 23:51:52 +0600

    s

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -4,6 +4,9 @@ const Home = () => {
     return (
         <div>
             <h1>This is Home Page</h1>
+            <h1>This is Home Page</h1>
+            <h1>This is Home Page</h1>
+            <h1>This is Home Page</h1>
         </div>
     );
 };
